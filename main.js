27b0b55c483a6a8b080915f55406ea69a0776fa3
Bruno Granemann <128817234+brunogranemann@users.noreply.github.com>
2023-06-14 09:21:16 -0300

    Finalizei a função de ativação das teclas através do clique do mouse

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); // Variável contendo uma lista com todas as teclas.
 
-listaDeTeclas[0].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador] // Variável contendo a tecla específica. Ex: <button class="tecla tecla_pom">Pom</button>
+    const instrumento = tecla.classList[1] // Contém a classe da tecla específica. Ex: tecla_pom
+    const idAudio = `#som_${instrumento}` // Contém uma string com o id da tag áudio. Ex: #som_tecla_pom
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
